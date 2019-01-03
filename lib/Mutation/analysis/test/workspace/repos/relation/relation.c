ae5f72b6ea51db35f49820f08c0333f1bb15deda
thierry <thierry_tct@yahoo.com>
2019-01-03 13:01:11 +0100

    klee

--- lib/Mutation/analysis/test/workspace/repos/relation/relation.c
+++ lib/Mutation/analysis/test/workspace/repos/relation/relation.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
- 
+#include <stdlib.h>
+int str2int(char * str)
+{
+  int x = 0;
+  if (str[0] >= '0' && str[0] <= '9')
+  {  
+      x = x*10 + str[0] - '0';
+      if (str[1] >= '0' && str[1] <= '9')
+      {
+        x = x*10 + str[1] - '0';
+        if (str[2] >= '0' && str[2] <= '9')
+        {
+          x = x*10 + str[1] - '0';
+        }
+      }
+      return x;
+  }
+  else
+  {
+    printf("str2int ERROR!");
+    exit(1);
+  }
+}
+
 int main(int argc, char **argv)
 {
   int a,b;
   int x;
  
-  printf("Enter two numbers\n");
-  scanf("%d %d", &a, &b);
+//  printf("Enter two numbers\n");
+//  scanf("%d %d", &a, &b);
+  a = str2int(argv[0]);
+  b = str2int(argv[1]);
  
   if (a > b)
     x = 1;
--- lib/Mutation/analysis/test/workspace/repos/winklee_bound/winklee_bound.c
+++ lib/Mutation/analysis/test/workspace/repos/winklee_bound/winklee_bound.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
- 
+#include <stdlib.h>
+int str2int(char * str)
+{
+  int x = 0;
+  if (str[0] >= '0' && str[0] <= '9')
+  {  
+      x = x*10 + str[0] - '0';
+      if (str[1] >= '0' && str[1] <= '9')
+      {
+        x = x*10 + str[1] - '0';
+        if (str[2] >= '0' && str[2] <= '9')
+        {
+          x = x*10 + str[1] - '0';
+        }
+      }
+      return x;
+  }
+  else
+  {
+    printf("str2int ERROR!");
+    exit(1);
+  }
+}
+
 int main(int argc, char **argv)
 {
   int a,b;
   int x;
  
   printf("Enter two numbers\n");
-  scanf("%d %d", &a, &b);
- 
+//  scanf("%d %d", &a, &b);
+  char str[4];
+  scanf ("%s", str);
+  a = str2int(str);
+  scanf ("%s", str);
+  b = str2int(str);
+
   x = 0;
   if (a > 5)
     x += a;
