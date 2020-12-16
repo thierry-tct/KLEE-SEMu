e55761d0743dd1573615c1b890f04b311f84c21e
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2020-12-16 16:47:51 +0100

    Update ExecutionState_KS.h

--- lib/Mutation/ExecutionState_KS.h
+++ lib/Mutation/ExecutionState_KS.h
@@ -209,7 +209,7 @@ public:
 #ifdef SEMU_RELMUT_PRED_ENABLED
   // record the version represented by the state
   // 0 means common to both old and new, -1 means old, 1 means new
-  char ks_old_new = 0
+  char ks_old_new = 0;
   
   // store the id of the point where the version split occued for this state 
   unsigned long long ks_oldnew_split_id;
